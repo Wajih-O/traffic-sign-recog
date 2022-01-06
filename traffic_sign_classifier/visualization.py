cd839c72c04aac426e0c07787c33a7ff37cf4fcf
Wajih Ouertani <wajih.ouertani@gmail.com>
2022-01-06 20:24:07 +0100

    update grid visualization

--- traffic_sign_classifier/visualization.py
+++ traffic_sign_classifier/visualization.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional
+from typing import Dict, Iterable, List, Optional
 
 import numpy as np
 import numpy.typing as npt
@@ -12,6 +12,7 @@ def grid_visu(
     images: np.ndarray,
     labels: List[int],
     sample_size: int = 3,
+    prediction_labels: Optional[List[int]] = None,
     categories: Optional[List[int]] = None,
     label_to_name: Optional[Dict[int, str]] = None,
     shuffle: bool = False,
@@ -28,6 +29,15 @@ def grid_visu(
     :param categories_per_fig: the number of categories to show/display per-figure (to better load/render)
     """
 
+    def show_image(ax, image: np.ndarray, title: Optional[str] = None):
+        """a helper to show/plot image in a subplot (AxesSubplot)"""
+        if idx is not None:
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+            ax.imshow(image)
+            if title:
+                ax.set_title(f"{title}", pad=-2)
+
     # Group the images by category/class
     by_category = group_by_category(labels, sample_size=sample_size, shuffle=shuffle)
 
@@ -39,16 +49,21 @@ def grid_visu(
         }
 
     sorted_categories = sorted(by_category.keys())  # sort selected categories
+    if not label_to_name:
+        label_to_name = {}
 
     # group categories in separate figures (seemd loading faster)
     for grp in [
         sorted_categories[i : i + categories_per_fig]
         for i in range(0, len(sorted_categories), categories_per_fig)
     ]:
-
         subfigs = plt.figure(
-            constrained_layout=True, figsize=(2 * sample_size, 2 * len(grp))
+            # constrained_layout=True,
+            figsize=(2 * sample_size, 2 * len(grp)),
+            tight_layout=False,
         ).subfigures(len(grp), 1, wspace=0.01)
+        if not isinstance(subfigs, Iterable):
+            subfigs = [subfigs]
 
         for category, fig in zip(grp, subfigs):
             # TODO: dynamically reshape the grid
@@ -56,12 +71,15 @@ def grid_visu(
                 fig,
                 111,
                 nrows_ncols=(1, len(by_category[category])),
-                axes_pad=0.3,
+                axes_pad=0.1,
             )
             for ax, idx in zip(grid, by_category[category]):
+                title = ""
                 if idx is not None:
-                    ax.get_xaxis().set_visible(False)
-                    ax.get_yaxis().set_visible(False)
-                    ax.imshow(images[idx, :])
-                    # ax.set_title(f"{idx}")
-            fig.suptitle(f"{label_to_name.get(category, category)}", fontsize="large")
+                    if prediction_labels:
+                        title = prediction_labels[idx]
+                    show_image(ax, images[idx], title)
+            fig.suptitle(
+                f"categ {category}:{label_to_name.get(category, category)}",
+                fontsize="large",
+            )
