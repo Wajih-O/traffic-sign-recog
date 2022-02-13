35b703299812809b47b33c1e4838cedd98f59738
Wajih Ouertani <wajih.ouertani@gmail.com>
2022-02-13 16:41:35 +0100

    move softmax to nn/utils.py

--- traffic_sign_classifier/nn/lenet.py
+++ traffic_sign_classifier/nn/lenet.py
@@ -24,20 +24,6 @@ from tensorflow.keras.losses import CategoricalCrossentropy
 from tensorflow.keras.optimizers import Optimizer
 
 
-def softmax(logits):
-    """A helper that computes softmax"""
-    exp_logits = np.exp(logits)
-    if len(exp_logits.shape) > 1:
-        # multiple image support
-        return (
-            exp_logits
-            / np.tile(np.sum(exp_logits, axis=1), (exp_logits.shape[1], 1)).transpose()
-        )
-    else:
-        # one image
-        return exp_logits / np.sum(exp_logits)
-
-
 def load_layers(layers: Optional[list] = None):
     if layers:
         return [layer for layer in layers]
---
+++ traffic_sign_classifier/nn/utils.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+def softmax(logits):
+    """A helper that computes softmax"""
+    exp_logits = np.exp(logits)
+    if len(exp_logits.shape) > 1:
+        # multiple image support
+        return (
+            exp_logits
+            / np.tile(np.sum(exp_logits, axis=1), (exp_logits.shape[1], 1)).transpose()
+        )
+    else:
+        # one image
+        return exp_logits / np.sum(exp_logits)
